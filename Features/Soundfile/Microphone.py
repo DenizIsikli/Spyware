4d5718e624be7d35410bf4be25801875f9e8dc4a
Bobby <s215818@dtu.dk>
2023-07-12 20:42:40 +0200

    Soundfile folder/feature

---
+++ Features/Soundfile/Microphone.py
@@ -0,0 +1,65 @@
+# Built-in modules
+import os
+import time
+
+# External libraries
+import pyaudio
+import wave
+
+# Custom module
+import Features.DataClass as Data_Class
+
+
+class Microphone(Data_Class):
+    def __init__(self):
+        self.baseclass = Data_Class.BaseClass.__init__(self)  # Call the base class __init__ method
+
+        # Constants for audio settings
+        self.end_time = self.baseclass.end_time
+        self.format = pyaudio.paInt16
+        self.channels = 2
+        self.sample_rate = 44100  # Normal wave length for normal sound quality
+        self.chunk = 1024
+        self.frames = []
+
+        # Generate a unique filename for the sub-folders
+        self.current_time = time.strftime("%Y%m%d-%H%M%S")
+
+        # Folder path and filename
+        self.folder_path = self.baseclass.folder_path_soundfile
+        self.output_file_name = f"Soundfile{self.current_time}.wav"
+
+    def audio_recording(self):
+        p = pyaudio.PyAudio()
+
+        stream = p.open(
+            format=self.format,
+            channels=self.channels,
+            rate=self.sample_rate,
+            input=True,
+            frames_per_buffer=self.chunk
+        )
+
+        while time.time() < self.end_time:
+            try:
+                data = stream.read(self.chunk)
+                self.frames.append(data)
+
+            except IOError as e:
+                print(f"Audio recording error: {e}")
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        # Create the folder path if it doesn't exist
+        if not os.path.exists(self.folder_path):
+            os.makedirs(self.folder_path)
+
+        # Save captured audio to the file path
+        file_path = os.path.join(self.folder_path, self.output_file_name)
+        with wave.open(file_path, 'wb') as f:
+            f.setnchannels(self.channels)
+            f.setsampwidth(p.get_sample_size(self.format))
+            f.setframerate(self.sample_rate)
+            f.writeframes(b''.join(self.frames))

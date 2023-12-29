eb5d49c85414bfcfb21f05e2eb8be8b1229c960b
Bobby <s215818@dtu.dk>
2023-12-29 02:41:02 +0100

    Direct use of /Features instead of env-var

--- Util/DataClass.py
+++ Util/DataClass.py
@@ -1,9 +1,5 @@
-# Built-in modules
-import datetime
 import os
 import time
-
-# External libraries
 from datetime import date, datetime
 from dataclasses import dataclass
 from dotenv import load_dotenv
@@ -21,32 +17,32 @@ class DataClass:
     gmail_address_sender: str = ""
     gmail_address_receiver: str = ""
 
-    currentDate = None
-    currentTime = None
-    duration = None
-    end_time = None
+    currentDate: date = date.today()
+    currentTime: datetime = datetime.now()
+    duration: int = 5
+    end_time: float = None
 
     def __init__(self):
+        self.script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.base_dir = os.path.join(self.script_dir, '..', 'config', 'config.env')
         self.create_paths()
-        self.base_dir = "/config/config.env"
+
+        self.end_time = time.time() + self.duration
 
     def create_paths(self):
         load_dotenv(dotenv_path=self.base_dir, verbose=True)
 
-        # Base directory
-        self.folder_path_cmdprompts = os.getenv("FOLDER_PATH_CMDPROMPTS")
-        self.folder_path_screenshot = os.getenv("FOLDER_PATH_SCREENSHOTS")
-        self.folder_path_soundfile = os.getenv("FOLDER_PATH_SOUNDFILE")
-        self.folder_path_statusupdate = os.getenv("FOLDER_PATH_STATUSUPDATE")
-        self.folder_path_systeminformation = os.getenv("FOLDER_PATH_SYS_INFO")
-        self.folder_path_webcamera = os.getenv("FOLDER_PATH_RECORDINGS")
+        # /Features
+        features_dir = os.path.join(self.script_dir, '..', 'Features')
+
+        # /Features/...
+        self.folder_path_cmdprompts = os.path.join(features_dir, "CmdPrompts")
+        self.folder_path_screenshot = os.path.join(features_dir, "Screenshot")
+        self.folder_path_soundfile = os.path.join(features_dir, "Soundfile")
+        self.folder_path_statusupdate = os.path.join(features_dir, "StatusUpdate")
+        self.folder_path_systeminformation = os.path.join(features_dir, "SystemInformation")
+        self.folder_path_webcamera = os.path.join(features_dir, "Webcamera")
 
         # Gmail
         self.gmail_address_sender = os.getenv("GMAIL_ADDRESS_SENDER")
         self.gmail_address_receiver = os.getenv("GMAIL_ADDRESS_RECEIVER")
-
-        # Config
-        self.currentDate = date.today()
-        self.currentTime = datetime.now()
-        self.duration = 300
-        self.end_time = time.time() + self.duration
